e1291a4e05bcdac8c0f8881d5af6987c7cd2decd
Den <denesh@u.nus.edu>
2023-03-24 15:08:56 +0800

    Adding tetrominos

--- src/helper/gameHelper.js
+++ src/helper/gameHelper.js
@@ -1,11 +1,13 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
-export const createState = () => {
+export const createStage = () => {
 
     // Create a new 2 by 2 array with a number and a state
     // the default state is 'clear'
-    Array.from(Array(STAGE_HEIGHT), () => {
-        new Array(STAGE_WIDTH).fill([0, 'clear'])
+    const return_array = Array.from(Array(STAGE_HEIGHT), () => {
+        return new Array(STAGE_WIDTH).fill([0, 'clear'])
     })
+
+    return return_array;
 }
